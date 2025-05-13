f3b4bb7dab496303972d6d0dad83cc617d68f6f9
Charlie Healy <550917@chrome.escg.ac.uk>
2025-05-13 11:49:45 +0100

    good progress

---
+++ Assets/Scripts/GoalPostScript.cs
@@ -0,0 +1,14 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalPostScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+            if (col.gameObject.CompareTag("Player"))
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+    }
+}
--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None; 
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
